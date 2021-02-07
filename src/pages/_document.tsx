6fbbd05e49716399bd56ed46111622e5bccbe3a3
Alexander L. de Goeij <mail@aldg.nl>
2021-02-07 14:47:10 +0100

    fix: SEO optimizations

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ import { GA_TRACKING_ID } from "../utils/gtag"
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
           {/* Global Site Tag (gtag.js) - Google Analytics */}
           <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
